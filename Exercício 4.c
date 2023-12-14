456fead00d56dc0c3839165ba9f3a532c575f6e3
Douglas <143917828+Zemiian@users.noreply.github.com>
2023-12-13 22:55:11 -0300

    Update Exercício 4.c

--- Exercício 4.c
+++ Exercício 4.c
@@ -2,41 +2,41 @@
 #include <stdlib.h>
 #include <locale.h>
 
-struct contaBancaria{
-    int numeroConta;
-    char nomeTitular[250];
+struct contaBancaria_{
+    int numeroDaConta;
+    char nomeDeTitular[250];
     float saldo;
     char tipoConta[250];
-}; 
+};
 
-struct contaBancaria Conta;
+struct contaBancaria_ Conta;
 
-struct contaBancaria depositar(struct contaBancaria conta, float valor){
-conta.saldo += valor;
- printf("\n Depósito de %.2f realizado",valor);
+struct contaBancaria_ depositar(struct contaBancaria_ conta, float valor){
+    conta.saldo += valor;
+    printf("Depósito de R$%.2f realizado\n",valor);
     return conta;
 }
 
-struct contaBancaria sacar(struct contaBancaria conta, float valor){
- if (conta.saldo >= valor){
- conta.saldo -= valor;
- printf("\n Saque de %.2f realizado",valor);
+struct contaBancaria_ sacar(struct contaBancaria_ conta, float valor){
+    if (conta.saldo >= valor){
+        conta.saldo -= valor;
+        printf("Saque de R$%.2f realizado\n",valor);
     } else {
-        printf("\n Saldo insuficiente para o saque.");
+        printf("Saldo insuficiente para o saque.\n");
     }
     return conta;
 }
 
-void saldo(struct contaBancaria conta){
-printf("\n Saldo atual: %2f",conta.saldo);
+void saldo(struct contaBancaria_ conta){
+    printf("Saldo atual: R$%.2f\n",conta.saldo);
 }
 
-void exibirMenu(){
-printf("\n 1 - Depositar");
-printf("\n 2 - Sacar");
-printf("\n 3 - Saldo"); 
-printf("\n 4 - Sair");
-printf("\n Escolha uma opção: ");
+void menu(){
+    printf("1 - Depositar\n");
+    printf("2 - Sacar\n");
+    printf("3 - Imprimir Saldo\n");
+    printf("4 - Sair\n");
+    printf("Escolha uma opção (1 a 4): ");
 }
 
 int main(){
@@ -46,36 +46,32 @@ int main(){
     float valor;
 
     do{
-    exibirMenu();
-    scanf("%i",&opcao);
-    
-	switch(opcao){
+    printf("\nEscolha o tipo de conta(Poupança ou corrente).\n");
+	gets(Conta.tipoConta);
+    menu();
+    scanf("%d", &opcao);
+	switch (opcao){
         case 1:
-        printf("Digite o valor do depósito: ");
+        printf("Digite o valor para depósito: ");
         scanf("%f", &valor);
         Conta = depositar(Conta, valor);
         break;
 
             case 2:
-            printf("Digite o valor se for sacar: ");
+            printf("Digite o valor para saque: ");
             scanf("%f", &valor);
             Conta = sacar(Conta, valor);
-break;
+            break;
 
             case 3:
             saldo(Conta);
             break;
-            
-            case 4:
-            	printf("Opção inválida. Tente novamente\n");
-            	break;
-            	
-            default:
-            printf("\n Opção inválida.");
-            break;
-};
 
-    } while(opcao != 4);
+            default:
+            printf("Opção inválida.\n");
+        }
+
+    } while (opcao >= 1 && opcao <= 3);
 
     return 0;
 }
